a42c4d452b3ceb2cbfb67e3241fe4ad976a1305e
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-29 00:11:23 +0300

    Webservice

--- CryptoRxSwift/Service/Webservice.swift
+++ CryptoRxSwift/Service/Webservice.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+
+enum WebserviceError: Error {
+    case serverError
+    case parsingError
+    
+}
+
+
+class Webservice {
+    
+    
+    func fetchData(url: URL,completion: @escaping (Result<[Crypto],Error>)->() ){
+        
+        
+        URLSession.shared.dataTask(with: url) { (data,response,error) in
+            if let _ =  error {
+                completion(.failure(WebserviceError.serverError))
+            } else if let data = data {
+                do {
+                    let cryptoData = try JSONDecoder().decode([Crypto].self, from: data)
+                    completion(.success(cryptoData))
+                } catch {
+                    completion(.failure(WebserviceError.parsingError))
+                }
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    
+}
