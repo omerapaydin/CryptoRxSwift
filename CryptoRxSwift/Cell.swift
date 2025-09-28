70b186141bafb6528966efa8fb767f01d3673297
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-29 00:51:23 +0300

    Cell

---
+++ CryptoRxSwift/Cell.swift
@@ -0,0 +1,23 @@
+//
+//  Cell.swift
+//  CryptoRxSwift
+//
+//  Created by Ömer on 29.09.2025.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
