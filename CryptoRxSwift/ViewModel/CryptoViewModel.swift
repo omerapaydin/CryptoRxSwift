a362ba87e5fbcaf5c43f82a7fdb7a5ac9bbd72e3
Ömer Apaydın <omerapaydin90@gmail.com>
2025-10-03 01:15:30 +0300

    VM

--- CryptoRxSwift/ViewModel/CryptoViewModel.swift
+++ CryptoRxSwift/ViewModel/CryptoViewModel.swift
@@ -6,21 +6,36 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class CryptoViewModel {
+    
+    
+    let cryptos : PublishSubject<[Crypto]> = PublishSubject()
+    let error : PublishSubject<String> = PublishSubject()
+    let loading : PublishSubject<Bool> = PublishSubject()
     
   
     func RequestData() {
         
+        self.loading.onNext(true)
+        
         let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!
         Webservice().downloadCurrencies(url: url) { cryptoResult in
-            
+            self.loading.onNext(false)
             switch cryptoResult {
             case .success(let cryptos):
-                print(cryptos)
+                self.cryptos.onNext(cryptos)
               
             case .failure(let error):
-                print(error)
+                switch error {
+                case .parsingError:
+                    self.error.onNext("Parsing Error")
+                case .serverError:
+                    self.error.onNext("Server Error")
+                
+                }
             }
         }
     }
